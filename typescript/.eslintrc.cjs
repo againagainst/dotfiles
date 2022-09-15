21fcb08bd7472c94a0d955267130ed7352927f1e
Sergey Belash <sergey.belash@wtw.no>
2022-09-15 15:21:09 +0200

    Typescript added nodemon and prettier

--- typescript/.eslintrc.cjs
+++ typescript/.eslintrc.cjs
@@ -1,9 +1,18 @@
 module.exports = {
-    env: {
-        node: true
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-  };
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "no-multi-spaces": "warn",
+    "no-multiple-empty-lines": "warn",
+    "no-unused-expressions": "warn",
+    "no-use-before-define": "warn",
+    "no-var": "warn",
+    "prefer-const": "warn",
+    "space-in-parens": "warn",
+  },
+};
